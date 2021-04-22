b1da7d7e9364c425e31a731d48ddd25848363e36
Ipan420 <81342478+Ipan420@users.noreply.github.com>
2021-04-22 10:50:42 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  this.sendContact(m.chat, '6282252655313', ' ᴍ ᴀꜰᴅʜᴀɴ || @dhans11__', m)
-m.reply('wa.me/6282252655313')
+  this.sendContact(m.chat, '6282286818715', ' IPAN-BOT || @Ipan4200', m)
+m.reply('wa.me/6282286818715')
 }
 
 handler.command = /^(owner|creator)$/i
