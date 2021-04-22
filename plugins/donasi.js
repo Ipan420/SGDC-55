71b6bae5e10fcc3d5bdf2baa1324c9ce1bdfd3f1
Ipan420 <81342478+Ipan420@users.noreply.github.com>
2021-04-22 11:45:16 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,14 @@
 let handler = async m => m.reply(`
-╭─────「 Donasi • SGDC-BOT 」
-│ • *Pulsa Telkomsel [082286818715]*
-│ • *Saldo Dana [082286818715]*
-│ • *Contact: wa.me/6282286818715*
-╰────────────────────────
+╭─────「 Donasi • IP-BOT 」─────
+│ ☞*Pulsa Telkomsel*
+│☞[082286818715]*
+│☞*Saldo Dana*
+│☞*[082286818715]*
+│☞*Contact*:
+│☞*wa.me/6282286818715*
+╰──────────────────────
 
- • *Contact: wa.me/6282286818715*
+ 
 `.trim())
 
 handler.command = /^dona(te|si)$/i
