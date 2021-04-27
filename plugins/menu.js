7a85be65362eecdd91d7044893f7bd751b5f3c2c
Ipan420 <81342478+Ipan420@users.noreply.github.com>
2021-04-27 13:47:01 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -53,7 +53,7 @@ let handler  = async (m, { conn, args, usedPrefix: _p }) => {
 _Hai,Kak @${mmk.split("@")[0]}_
 
 *☞Owner Ipan Gans:v*
-☞Wa.me/6282286818715
+*☞Wa.me/6282286818715*
 *╭══════════════════╮*
 ❍ *Total Pengguna ${bname}* ❍
 ❏ ~> ${reg} User
