53e183192bbed5824d5aebda7d65d26d1719e29a
Ipan420 <81342478+Ipan420@users.noreply.github.com>
2021-04-28 11:40:02 +0700

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  this.sendContact(m.chat, '6282286818715', 'IPAN IP-BOT', m)
+  this.sendContact(m.chat, '6282286818715', 'IPAN GANS:V', m)
 //let pp_owner ='src/Owner.jpg'
 //conn.sendFile(m.chat, pp_owner, 'My Owner.jpg', '*This is my owner ᴍ ᴀꜰᴅʜᴀɴ*\n*Contact: https://wa.me/6282252655313*', m)
 }
