291cc20f6c36984fc728b05f7b5b3144274544dd
Ipan420 <81342478+Ipan420@users.noreply.github.com>
2021-04-29 13:31:32 +0700

    Update Joinn.js

--- plugins/Joinn.js
+++ plugins/Joinn.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text }) => {
     //let name = m.fromMe ? conn.user : conn.contacts[m.sender]
-    let own = '6282252655313@s.whatsapp.net'
+    let own = '6282286818715@s.whatsapp.net'
   conn.reply(m.chat, `_Ngapain Manggil Owner @${own.split("@")[0]}?_`, m, { 
       contextInfo: { 
           mentionedJid: [own] 
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text }) => {
   })
     //let mentionedJid = [m.sender]
 }
-handler.customPrefix = /^(@꧁࿇ ᴍ ᴀꜰᴅʜᴀɴ ࿇꧂|Afdhan|@6282252655313)/i
+handler.customPrefix = /^(@IPAN GANS:V|IPAN|@6282286818715)/i
 handler.command = new RegExp
 
 module.exports = handler				     
