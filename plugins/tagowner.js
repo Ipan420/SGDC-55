3cf2d10500ad136a1827001e0d48e5b1e6d29031
Ipan420 <81342478+Ipan420@users.noreply.github.com>
2021-04-29 13:39:06 +0700

    Update tagowner.js

--- plugins/tagowner.js
+++ plugins/tagowner.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-*Ngapain Manggil ꧁࿇ ᴍ ᴀꜰᴅʜᴀɴ ࿇꧂?*
+*Ngapain Manggil ꧁࿇ IPAN ࿇꧂?*
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /^(@꧁࿇ ᴍ ᴀꜰᴅʜᴀɴ ࿇꧂|@꧁࿇ M AFDHAN ࿇꧂|Afdhan)$/i
+handler.customPrefix = /^(@꧁࿇ IPAN ࿇꧂|@꧁࿇ IPAN ࿇꧂Pan)$/i
 handler.command = new RegExp
 
 module.exports = handler
