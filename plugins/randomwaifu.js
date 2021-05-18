e019b0cdd8352b255752209d3c9bb3c45c0ba993
Ipan420 <81342478+Ipan420@users.noreply.github.com>
2021-05-18 13:48:27 +0700

    Update randomwaifu.js

--- plugins/randomwaifu.js
+++ plugins/randomwaifu.js
@@ -5,7 +5,7 @@ let chat = global.DATABASE.data.chats[m.chat]
     if (chat.nsfw) { 
     let api = (kntl.lolkey)
   	await m.reply(global.wait)
-      let link = 'https://api.xteam.xyz/anime/waifu?APIKEY=9c0fb74ad8c539e3' + api;
+      let link = 'https://api.xteam.xyz/anime/waifu?APIKEY=9c0fb74ad8c539e3';
       conn.sendFile(m.chat, link, 'SGDC-BOT.png', 'No Colay:v\n\n*SGDC-BOT*', m)
     } else {
        m.reply('```Perlu Mengaktifkan Mode NSFW```')
